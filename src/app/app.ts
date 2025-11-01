5751920a5a08159ca8c6822d8581cba516666e6d
Rishit-kevit <rishit.rajpara@kevit.io>
2025-11-01 19:10:16 +0530

    Initial setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ButtonModule, CardModule, InputTextModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected readonly title = signal('angular-playground');
-}
+export class AppComponent { }
